e32b6fc69b0545d55e53e2ad465258dcc6bb014d
Greg Bird <greg.bird@joltup.com>
2018-01-18 16:03:59 -0700

    finish django social auth and very simple login page

--- home/views.py
+++ home/views.py
@@ -5,11 +5,10 @@ from django.contrib.auth import logout as auth_logout
 def index(request):
     return render(request, 'index.html')
 
-# Create your views here.
-
-
-
 def logout(request):
     """Logs out a user"""
     auth_logout(request)
     return render_to_response('index.html')
+
+def login(request):
+    return render(request, 'login.html')
