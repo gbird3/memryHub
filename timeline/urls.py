b145aaa3bfb50809e4c3ec25e1a25d9e6a2790dc
Greg Bird <greg.bird@joltup.com>
2018-01-23 09:48:13 -0700

    add initial timeline structure

--- timeline/urls.py
+++ timeline/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.timelines, name='timelines'),
+    path('create', views.create, name='create'),
     path('view', views.view, name='view'),
 ]
--- timeline/views.py
+++ timeline/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def timelines(request):
+    return render(request, 'timeline.html')
+
+
+def create(request):
+    return render(request, 'create.html')
+
+
 def view(request):
     return render(request, 'view.html')
