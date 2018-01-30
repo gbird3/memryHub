58c12c3557232bbe44ffefd69c9f0fc8132fe8a2
Greg Bird <greg.bird@joltup.com>
2018-01-30 13:13:50 -0700

    add Memory model

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Timeline, Card
+from .models import Timeline, Card, Memory
 # Register your models here.
 
 admin.site.register(Timeline)
 admin.site.register(Card)
+admin.site.register(Memory)
