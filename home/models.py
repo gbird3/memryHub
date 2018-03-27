4d129d9021a75b6ede0f021a70338c165bbc831c
Greg Bird <gregbird12@gmail.com>
2018-03-27 17:37:06 -0600

    Group model and logic for creating groups and adding users (#63)
    * initial groups work
    
    * add groups and group users pages. Add ability to create groups and add users
    
    * remove commented code
    
    * add alert about groups being in beta

--- home/models.py
+++ home/models.py
@@ -6,3 +6,21 @@ class UserInfo(models.Model):
     '''Stores the information on the user's root google folder where we will store their files'''
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     root_folder_id = models.CharField(null=True, blank=True, max_length=300)
+
+class Group(models.Model):
+    '''A group for users'''
+    name = models.CharField(null=True, max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    active = models.SmallIntegerField(default=1)
+
+    def sumUsers(self):
+        return UserHasGroup.objects.filter(group=self).count()
+    
+    users = property(sumUsers)
+
+class UserHasGroup(models.Model):
+    '''Users that belong to a group'''
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
